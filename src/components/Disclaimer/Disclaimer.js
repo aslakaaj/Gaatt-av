fb3e4f6642d34d95b11a3718254b49d5fc4339e3
Aslak Aarflot Jønsson <aslakaaj@gmail.com>
2023-10-19 20:04:48 +0200

    Made som styling to disclaimer

--- src/components/Disclaimer/Disclaimer.js
+++ src/components/Disclaimer/Disclaimer.js
@@ -1,4 +1,4 @@
-import "./Disclaimer.css";
+import styles from "./Disclaimer.module.css";
 import Button from "../UI/Button.js";
 
 const Disclaimer = (props) => {
@@ -6,16 +6,17 @@ const Disclaimer = (props) => {
     props.onClickChange(false);
   };
   return (
-    <div className="disclaimer-box">
+    <div className={styles["disclaimer-box"]}>
       <h2>Ansvarsfraskrivelse</h2>
-      <p className="disclaimer-content">
+      <p className={styles["disclaimer-content"]}>
         Jeg er et uavhengig enkeltindivid som ikke er tilknyttet noe form for
         avis eller nyhetsbyrå, og har har laget nettstedet av egen interesse og
         hobby. Vil påpeke at jeg imidlertid er medlem av Unge Høyre, men har
         ikke latt politiske syn formet hvordan jeg har formulert beskrivelsene
         til statsrådene. Mener du der er feil knyttet til fakta, eller andre
-        innvendinger? <span>Kontakt meg på aslakaaj@gmail.com</span>
+        innvendinger? Ta kontakt.
       </p>
+      <p className={styles.contact}><a href="mailto: aslakaaj@gmail.com">Kontakt meg på aslakaaj@gmail.com</a></p>
       <Button onClick={clickHandler}>Tilbake...</Button>
     </div>
   );
--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,8 +1,8 @@
 import styles from "./Button.module.css";
 
 const Button = (props) => {
-    // style={this.style.backgroundColor = document.body.style.backgroundColor} 
-    return <button className={styles["custom-btn"]} onClick={props.onClick}>{props.children}</button>;
+    //style={this.style.backgroundColor = document.body.style.backgroundColor} 
+    return <button className={styles["custom-btn"]} style={{backgroundColor: document.body.style.backgroundColor}}onClick={props.onClick}>{props.children}</button>;
 }
 
 export default Button;
